3b757dc837ad3d48b92459d063e70b061622d267
ricsoh <ricsoh@gmail.com>
2020-12-14 13:59:17 +0800

    useState for bus stop and number

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=43009";
-
 export default function App() {
   const [loading, setLoading] = useState(true);
   const [arrival, setArrival] = useState("");
   const [busNumber, setBusNumber] = useState("945");
+  const [busStop, setBusStop] = useState("43009");
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=" + busStop;
+
 
   // This will retrive data using API
   function loadBusStopData() {
@@ -20,19 +21,16 @@ export default function App() {
       .then((responseData) => {
         console.log("Bus numer:" + busNumber); // display data at console
         const myBus = responseData.services.filter(
-//          (item) => item.no === "945"
           (item) => item.no === busNumber
         )[0];
         setArrival(myBus.next.time);
         setLoading(false);
-//        console.log("My bus:");
-//        console.log(myBus);
       });
   }
 
   // This happened once
   useEffect(() => {
-    const interval = setInterval(loadBusStopData, 1000);
+    const interval = setInterval(loadBusStopData, 3000);
 
     // Return the function to run when unmouting
     return () => clearInterval(interval);
@@ -43,43 +41,50 @@ export default function App() {
   }
 
  return (
-   <View style={styles.container}>
-     <Text style={styles.title}>Bus ({busNumber}) arrival time:</Text>
-      <Text style={styles.arrivalTime}>
-        {loading ? <ActivityIndicator size="large" color="blue"/> : arrival}
-      </Text>
-     <TouchableOpacity style={styles.button} onPress={() => refreshPressed()}>
-       <Text style={styles.buttonText}>Refresh!</Text>
-     </TouchableOpacity>
-   </View>
+  <View style={styles.container}>
+    <Text style={styles.title}>Bus stop</Text>
+    <Text style={styles.arrivalInfo}>{busStop}</Text>
+    <Text style={styles.title}>Bus number</Text>
+    <Text style={styles.arrivalInfo}>{busNumber}</Text>
+    <Text style={styles.title}>Bus arrival time</Text>
+    <Text style={styles.arrivalInfo}>
+      {loading ? <ActivityIndicator size="large" color="blue"/> : arrival}
+    </Text>
+    <TouchableOpacity style={styles.button} onPress={() => refreshPressed()}>
+      <Text style={styles.buttonText}>Refresh!</Text>
+    </TouchableOpacity>
+  </View>
  );
 }
 
 const styles = StyleSheet.create({
  container: {
-   flex: 1,
-   backgroundColor: "#fff",
-   alignItems: "center",
-   justifyContent: "center",
+  flex: 1,
+  backgroundColor: "#fff",
+  alignItems: "center",
+  justifyContent: "center",
  },
- title: {
-   fontWeight: "bold",
-   fontSize: 32,
-   marginBottom: 24,
+ title: { 
+  fontWeight: "bold",
+  fontSize: 26,
+  color: "blue",
+  textAlign: "left",
+  marginBottom: 24,
+  textDecorationLine: "underline",
  },
- arrivalTime: {
-   fontSize: 40,
-   marginBottom: 32,
+ arrivalInfo: {
+  fontSize: 20,
+  marginBottom: 30,
  },
  button: {
-   padding: 20,
-   backgroundColor: "darkgreen",
-   borderRadius: 10,
+  padding: 20,
+  backgroundColor: "darkgreen",
+  borderRadius: 10,
  },
  buttonText: {
-   fontSize: 32,
-   fontWeight: "bold",
-   color: "white",
+  fontSize: 28,
+  fontWeight: "bold",
+  color: "white",
  },
 });
 
